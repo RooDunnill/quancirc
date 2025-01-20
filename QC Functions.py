3db8a85a715b9c256bee74a7445c8f073cdf4b62
Roo <dunnillt@gmail.com>
2025-01-20 21:27:15 +0000

    edited the tensor and mat mult functions to be able to combine Gates with symbols that can then allow multiple combinations in one

--- QC Functions.py
+++ QC Functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sp
+import random as rm
 sp.init_printing(use_unicode=True)
 
 
@@ -11,36 +12,38 @@ class Qubit:
     def __str__(self):
         return f"{self.name}({self.vector})"
     
-    def tensor_prod(self, other):
-        new_name = f"{self.name} X {other.name}"
-        self_length = len(self.vector)
-        other_length = len(other.vector)
-        new_length = self_length*other_length
-        new_vector = np.zeros(new_length,dtype=np.complex128)
-        for i in range(self_length):
-            for j in range(other_length):
-                new_vector[j+i*other_length] += self.vector[i]*other.vector[j]
-        sp.simplify(new_vector)
-        return Qubit(new_name, np.array(new_vector))
-    
+    def __matmul__(self, other):
+        if isinstance(other, Qubit):
+            new_name = f"{self.name} X {other.name}"
+            self_length = len(self.vector)
+            other_length = len(other.vector)
+            new_length = self_length*other_length
+            new_vector = np.zeros(new_length,dtype=np.complex128)
+            for i in range(self_length):
+                for j in range(other_length):
+                    new_vector[j+i*other_length] += self.vector[i]*other.vector[j]
+            sp.simplify(new_vector)
+            return Qubit(new_name, np.array(new_vector))
+        
     def norm(self):
         normalise = sp.sqrt(sum([i*np.conj(i) for i in self.vector]))
         self.vector = sp.simplify(self.vector/normalise)
 
 
     def measure(self):
+
         pass
 
 q0_matrix = [1,0]      
 q1_matrix = [0,1]
 
-q0 = Qubit("q1",q0_matrix)
-q1 = Qubit("q2",q1_matrix)
+q0 = Qubit("q0",q0_matrix)
+q1 = Qubit("q1",q1_matrix)
 q0.norm()
 q1.norm()
-q3 = q1.tensor_prod(q0)
-#print(q1,q0)
-#print(q3)
+q3 = q1 @ q0
+print(q1,q0)
+print(q3)
 
 class Gate:
     def __init__(self, name, matrix):
@@ -50,37 +53,52 @@ class Gate:
     def __str__(self):
         return f"{self.name}({self.matrix})"
 
-    def tensor_prod(self, other):
-        new_name = f"{self.name} X {other.name}"
-        self_length = len(self.matrix)
-        self_dim = sp.sqrt(self_length)
-        other_length = len(other.matrix)
-        other_dim = sp.sqrt(other_length)
-        new_length = self_length*other_length
-        new_dim = sp.sqrt(new_length)
-        new_mat = np.zeros(new_length,dtype=np.complex128)
-        for m in range(self_dim):
-            for i in range(self_dim):
-                for j in range(other_dim):
-                    for k in range(other_dim):
-                        new_mat[k+j*new_dim+other_dim*i+other_dim*new_dim*m] += self.matrix[i+self_dim*m]*other.matrix[k+other_dim*j]
-        sp.simplify(new_mat)
+    def __matmul__(self, other):
+        if isinstance(other, Gate):
+            new_name = f"{self.name} X {other.name}"
+            self_length = len(self.matrix)
+            self_dim = sp.sqrt(self_length)
+            other_length = len(other.matrix)
+            other_dim = sp.sqrt(other_length)
+            new_length = self_length*other_length
+            new_dim = sp.sqrt(new_length)
+            new_mat = np.zeros(new_length,dtype=np.complex128)
+            for m in range(self_dim):
+                for i in range(self_dim):
+                    for j in range(other_dim):
+                        for k in range(other_dim):
+                            new_mat[k+j*new_dim+other_dim*i+other_dim*new_dim*m] += self.matrix[i+self_dim*m]*other.matrix[k+other_dim*j]
+            sp.simplify(new_mat)
         return Gate(new_name, np.array(new_mat))
 
-    def gate_mult(self, other):
-        new_name = f"{self.name} x {other.name}"
+    def __mul__(self, other):
         length = len(self.matrix)
-        dim = sp.sqrt(len(self.matrix))
-        new_mat = np.zeros(length,dtype=np.complex128)
+        dim = int(sp.sqrt(len(self.matrix)))
         summ = np.zeros(1,dtype=np.complex128)
-        for i in range(dim):
-            for k in range(dim):
-                for j in range(dim):
-                    summ[0] += (self.matrix[j+dim*i]*other.matrix[k+j*dim])
-                new_mat[k+dim*i] += summ[0]
-                summ = np.zeros(1,dtype=np.complex128)
-        sp.simplify(new_mat)
-        return Gate(new_name, np.array(new_mat))
+        if isinstance(other, Gate):
+            new_name = f"{self.name} * {other.name}"
+            new_mat = np.zeros(length,dtype=np.complex128)
+            for i in range(dim):
+                for k in range(dim):
+                    for j in range(dim):
+                        summ[0] += (self.matrix[j+dim*i]*other.matrix[k+j*dim])
+                    new_mat[k+dim*i] += summ[0]
+                    summ = np.zeros(1,dtype=np.complex128)
+            sp.simplify(new_mat)
+            return Gate(new_name, np.array(new_mat))
+        elif isinstance(other, Qubit):
+            new_name = f"{self.name} {other.name}"
+            
+            new_mat = np.zeros(dim,dtype=np.complex128)
+            for i in range(dim):
+                    for j in range(dim):
+                        summ[0] += (self.matrix[j+dim*i]*other.vector[j])
+                    new_mat[i] += summ[0]
+                    summ = np.zeros(1,dtype=np.complex128)
+            return Qubit(new_name, np.array(new_mat))
+
+
+
 
 
 
@@ -93,45 +111,13 @@ Y_matrix = [0,np.complex128(0-1j),np.complex128(0+1j),0]
 Y_Gate = Gate("Y_Gate", Y_matrix)
 Z_matrix = [1,0,0,-1]
 Z_Gate = Gate("Z Gate", Z_matrix)
-Hadamard_matrix = [1,1,1,-1]
+n = 1/sp.sqrt(2)
+Hadamard_matrix = [n,n,n,-n]
 Hadamard = Gate("Hadamard", Hadamard_matrix)
 #print(Hadamard.gate_mult(X_Gate))
 #print(Hadamard.gate_mult(Y_Gate))
 #print(C_Not.tensor_prod(Hadamard))
-
-
-
-
-class Circuit:
-    def __init__(self, name, circuit_grid):
-        self.name = name
-        self.circuit_grid = np.array(circuit_grid,dtype=np.complex128)
-
-    def __str__(self):
-        return f"{self.name}({self.circuit_grid})"
-    gate_key = {1:X_Gate, 2:Y_Gate, 2:Z_Gate}
-
-
-    def circuit_compiler(self):
-        num_qubits = len(circuit_grid)
-        circuit_length = int(sum(len(p) for p in self.circuit_grid)/num_qubits)
-        g = 0
-        while g < circuit_length:
-            
-
-
-oneqcircuit = [1,2,3]
-twoqcircuit = np.array([[1,2,3],
-                        [1,2,3]])
-
-""" KEY FOR THE CIRCUIT
-    0 = Nothing/Empty
-    1 = X Gate
-    2 = Y Gate
-    3 = Z Gate
-    4 = Hadamard
-    5 = Control X Gate
-    6 = Control Y Gate
-    7 = Control Z Gate
-    8 = The control qubit in the gate """
-
+#print(Hadamard @ X_Gate @ X_Gate)
+#print(Hadamard * X_Gate * X_Gate)
+#print(Hadamard*q1)
+print(Hadamard*Hadamard*q1)
