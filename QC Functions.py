349a5dfb3b13b1346d05ad983121beace9ec9d83
Roo <dunnillt@gmail.com>
2025-01-20 16:14:40 +0000

    created the gate multiplier

--- QC Functions.py
+++ QC Functions.py
@@ -4,35 +4,44 @@ import sympy as sp
 sp.init_printing(use_unicode=True)
 
 class Gate:
-    def __init__(self, name, a, b, c, d):
+    def __init__(self, name, matrix):
         self.name = name
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+        self.matrix = np.array(matrix)
 
     def __str__(self):
-        return f"{self.name}({self.a},{self.b})({self.c},{self.d})"
+        return f"{self.name}({self.matrix})"
 
     def norm(self):
-        a = self.a
-        b = self.b
-        c = self.c
-        d = self.d
-        norm = sp.sqrt(a**2 + b**2 + c**2 + d**2)
-        self.a = a / norm
-        self.b = b / norm
-        self.c = c / norm
-        self.d = d / norm
-        self.a = sp.simplify(self.a)
-        self.b = sp.simplify(self.b)
-        self.c = sp.simplify(self.c)
-        self.d = sp.simplify(self.d)
-        
+        normalise = sp.sqrt(sum([i**2 for i in self.matrix]))
+        self.matrix = sp.simplify(self.matrix/normalise)
 
-Hadamard = Gate("Hadamard", 1,1,1,-1)
-Hadamard.norm()
-print(Hadamard)
+    def gate_mult(self, other):
+        new_name = f"{self.name} {other.name}"
+        length = len(self.matrix)
+        dim = sp.sqrt(len(self.matrix))
+        new_mat = []
+        summ = 0
+        for i in range(dim):
+            for k in range(dim):
+                for j in range(dim):
+                    summ += (self.matrix[j+dim*i]*other.matrix[k+j*dim])
+                print(summ)
+                new_mat.append(summ)
+                summ = 0
+        print(new_mat)
+        return Gate(new_name, np.array(new_mat))
+
+
+
+
+C_Not_matrix = [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0]
+C_Not = Gate("C_Not", C_Not_matrix)
+X_matrix = [0,1,1,0]
+X_matrix = Gate("X_Matrix", X_matrix)
+Hadamard_matrix = [1,1,1,-1]
+Hadamard = Gate("Hadamard", Hadamard_matrix)
+
+
 class Qubit:
     def __init__(self, name, a, b):
         self.name = name
