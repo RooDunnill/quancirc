16341e0e599b14332caa15f0bc30a87d85c9b635
Roo <dunnillt@gmail.com>
2025-01-21 12:36:47 +0000

    added a simple algorithm function for compliing the matrices

--- QC Functions.py
+++ QC Functions.py
@@ -3,11 +3,11 @@ import sympy as sp
 import random as rm
 sp.init_printing(use_unicode=True)
 
-
 class Qubit:
     def __init__(self, name, vector):
         self.name = name
         self.vector = np.array(vector,dtype=np.complex128)
+        
 
     def __str__(self):
         return f"{self.name}({self.vector})"
@@ -46,9 +46,10 @@ print(q1,q0)
 print(q3)
 
 class Gate:
-    def __init__(self, name, matrix):
+    def __init__(self, name, info, matrix):
         self.name = name
         self.matrix = np.array(matrix,dtype=np.complex128)
+        self.info = info
 
     def __str__(self):
         return f"{self.name}({self.matrix})"
@@ -69,7 +70,7 @@ class Gate:
                         for k in range(other_dim):
                             new_mat[k+j*new_dim+other_dim*i+other_dim*new_dim*m] += self.matrix[i+self_dim*m]*other.matrix[k+other_dim*j]
             sp.simplify(new_mat)
-        return Gate(new_name, np.array(new_mat))
+        return Gate(new_name, self.info, np.array(new_mat))
 
     def __mul__(self, other):
         length = len(self.matrix)
@@ -85,9 +86,9 @@ class Gate:
                     new_mat[k+dim*i] += summ[0]
                     summ = np.zeros(1,dtype=np.complex128)
             sp.simplify(new_mat)
-            return Gate(new_name, np.array(new_mat))
+            return Gate(new_name, self.info, np.array(new_mat))
         elif isinstance(other, Qubit):
-            new_name = f"{self.name} {other.name}"
+            new_name = f"[{self.name}] {other.name}"
             
             new_mat = np.zeros(dim,dtype=np.complex128)
             for i in range(dim):
@@ -96,6 +97,15 @@ class Gate:
                     new_mat[i] += summ[0]
                     summ = np.zeros(1,dtype=np.complex128)
             return Qubit(new_name, np.array(new_mat))
+    
+    def gate_info(self):
+        print(
+    """Gates are used to apply an operation to a Qubit.
+    They are normally situated on a grid of n Qubits.
+    Using tensor products, we can combine all the gates 
+    at one time instance together to create one unitary matrix.
+    Then we can matrix multiply successive gates together to creat one
+    universal matrix that we can apply to the Qubit before measuring""")
 
 
 
@@ -103,21 +113,45 @@ class Gate:
 
 
 
+C_Not_info = "test"
 C_Not_matrix = [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0]
-C_Not = Gate("C_Not", C_Not_matrix)
+C_Not = Gate("C_Not", C_Not_info, C_Not_matrix)
+X_Gate_info = "test"
 X_matrix = [0,1,1,0]
-X_Gate = Gate("X_Gate", X_matrix)
+X_Gate = Gate("X", X_Gate_info, X_matrix)
+Y_Gate_info = "test"
 Y_matrix = [0,np.complex128(0-1j),np.complex128(0+1j),0]
-Y_Gate = Gate("Y_Gate", Y_matrix)
+Y_Gate = Gate("Y",Y_Gate_info, Y_matrix)
+Z_Gate_info = "test"
 Z_matrix = [1,0,0,-1]
-Z_Gate = Gate("Z Gate", Z_matrix)
+Z_Gate = Gate("Z",Z_Gate_info, Z_matrix)
+Identity_info = """
+Identity Matrix: This matrix leaves the product invariant after multiplication.
+It is mainly used in this program to increase the dimension
+of other matrices. This is used within the tensor products when
+a Qubit has no gate action, but the others do.
+"""
+Identity_matrix = [1,0,0,1]
+Identity = Gate("I",Identity_info, Identity_matrix)
 n = 1/sp.sqrt(2)
+Hadamard_info = "test"
 Hadamard_matrix = [n,n,n,-n]
-Hadamard = Gate("Hadamard", Hadamard_matrix)
+Hadamard = Gate("H",Hadamard_info, Hadamard_matrix)
 #print(Hadamard.gate_mult(X_Gate))
 #print(Hadamard.gate_mult(Y_Gate))
 #print(C_Not.tensor_prod(Hadamard))
 #print(Hadamard @ X_Gate @ X_Gate)
 #print(Hadamard * X_Gate * X_Gate)
 #print(Hadamard*q1)
-print(Hadamard*Hadamard*q1)
+#print(Hadamard*Hadamard*q1)
+#print(Identity.gate_info)
+
+def Test_Alg(Qubit):
+    gate1 = X_Gate @ C_Not
+    gate2 = Hadamard @ Hadamard @ X_Gate
+    gate3 = C_Not @ X_Gate
+    alg = gate3 * gate2 * gate1
+    result = alg * Qubit
+    print(result)
+    pass
+Test_Alg(q1 @ q1 @ q0)
