f8d42a36e9faf7fd25d4babe5a42c2209fc0015f
Roo <dunnillt@gmail.com>
2025-01-21 23:16:09 +0000

    finally commented cause i am such a profesh programmer

--- QC Functions.py
+++ QC Functions.py
@@ -1,20 +1,21 @@
-import numpy as np
-import sympy as sp
-import random as rm
-sp.init_printing(use_unicode=True)
+import numpy as np                                                            #mostly used to make 1D arrays
+import sympy as sp                                              #mostly used for sqrt function, and simplify
+import random as rm                                             #used for measuring
+sp.init_printing(use_unicode=True)                              #pretty much useless i think
 
-class Gate_data:
-    C_Not_info = "test"
-    C_Not_matrix = [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0]
+class Gate_data:                    #defines a class to store variables in to recall from so that its all
+    C_Not_info = "test"                                          #in one neat area
+    C_Not_matrix = [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0]#2 Qubit CNot gate in one configuration, need to add more
     X_Gate_info = "test"
-    X_matrix = [0,1,1,0]
+    X_matrix = [0,1,1,0]                           
     Y_Gate_info = "test"
-    Y_matrix = [0,np.complex128(0-1j),np.complex128(0+1j),0]
+    Y_matrix = [0,np.complex128(0-1j),np.complex128(0+1j),0]           
+    Z_Gate_info = "test"
+    Z_matrix = [1,0,0,-1]                                      
     n = 1/sp.sqrt(2)
     Hadamard_info = "test"
     Hadamard_matrix = [n,n,n,-n]
-    Z_Gate_info = "test"
-    Z_matrix = [1,0,0,-1]
+    
     Identity_info = """
 Identity Matrix: This matrix leaves the product invariant after multiplication.
 It is mainly used in this program to increase the dimension
@@ -28,48 +29,46 @@ a Qubit has no gate action, but the others do.
 
 
 
-class Qubit:
+class Qubit:                        
     def __init__(self, name, vector):
         self.name = name
         self.vector = np.array(vector,dtype=np.complex128)
-        self.dim = len(vector)
+        self.dim = len(vector)                    #used constantly in all calcs so defined it universally
         
 
     def __str__(self):
-        return f"{self.name}\n{self.vector}"
+        return f"{self.name}\n{self.vector}"   #did this so that the matrix prints neatly
     
-    def __matmul__(self, other):
-        if isinstance(other, Qubit):
-            new_name = f"{self.name} @ {other.name}"
+    def __matmul__(self, other):               #this is an n x n tensor product function
+        if isinstance(other, Qubit):           #although this tensors are all 1D
+            new_name = f"{self.name} @ {other.name}"     #no tensor symbol so this will do
             new_length = self.dim*other.dim
             new_vector = np.zeros(new_length,dtype=np.complex128)
-            for i in range(self.dim):
-                for j in range(other.dim):
-                    new_vector[j+i*other.dim] += self.vector[i]*other.vector[j]
-            sp.simplify(new_vector)
-            return Qubit(new_name, np.array(new_vector))
+            for i in range(self.dim):     #multiplies the second ket by each value in the first ket
+                for j in range(other.dim):          #iterates up and down the second ket
+                    new_vector[j+i*other.dim] += self.vector[i]*other.vector[j] #adds the values into
+            sp.simplify(new_vector)                           #a new matrix in order
+            return Qubit(new_name, np.array(new_vector))    #returns a new Object with a new name too
         else:
             Print(Gate_data.operation_error)
 
 
-    def norm(self):
+    def norm(self):                 #dunno why this is here ngl, just one of the first functions i tried
         normalise = sp.sqrt(sum([i*np.conj(i) for i in self.vector]))
         self.vector = sp.simplify(self.vector/normalise)
 
-    def qubit_info(self):
+    def qubit_info(self):           #will get to it one day
 
         pass
 
 
     def measure(self, qubit):
-        if qubit <= self.dim:
-            qubit_choice = [0,1]
+        if qubit <= self.dim:    
+            qubit_choice = [0,1]   #this array is for the random.choice to pick an outcome
             a = np.complex128(self.vector[2*qubit-2]*np.conj(self.vector[2*qubit-2]))
             b = np.complex128(self.vector[2*qubit-1]*np.conj(self.vector[2*qubit-1]))
-            print(a)
-            print(b)
-            prob_mat = np.array([a,b],dtype=np.complex128)
-            measurement = rm.choices(qubit_choice,weights = prob_mat)
+            prob_mat = np.array([a,b],dtype=np.complex128) #couldnt put straight into matrix as 
+            measurement = rm.choices(qubit_choice,weights = prob_mat)     #returned an error
             return measurement
         else:
             print(Gate_data.operation_error)
@@ -95,7 +94,7 @@ class Gate:
         self.name = name
         self.matrix = np.array(matrix,dtype=np.complex128)
         self.info = info
-        self.length = len(matrix)
+        self.length = len(matrix)          #naming these matrices and qubits vectors was a stupid idea XD
         self.dim = int(sp.sqrt(self.length))
         
 
@@ -112,23 +111,23 @@ class Gate:
             for m in range(self.dim):
                 for i in range(self.dim):
                     for j in range(other.dim):
-                        for k in range(other.dim):
+                        for k in range(other.dim):   #honestly, this works but is trash and looks like shit
                             new_mat[k+j*new_dim+other.dim*i+other.dim*new_dim*m] += self.matrix[i+self.dim*m]*other.matrix[k+other.dim*j]
-            sp.simplify(new_mat)
+            sp.simplify(new_mat)                     #will try to impliment a XOR operation for this which should be a lot fater
             return Gate(new_name, new_info, np.array(new_mat))
         else:
             print(Gate_data.operation_error)
 
-    def __mul__(self, other):
-        summ = np.zeros(1,dtype=np.complex128)
-        if isinstance(other, Gate):
+    def __mul__(self, other):       #matrix multiplication
+        summ = np.zeros(1,dtype=np.complex128)  #could delete summ and make more elegant
+        if isinstance(other, Gate):    #however probs completely better way to do this so might scrap at some point
             if self.dim == other.dim:
                 new_info = "This is a matrix multiplication of gates: "f"{self.name}"" and "f"{other.name}"
                 new_name = f"{self.name} * {other.name}"
                 new_mat = np.zeros(self.length,dtype=np.complex128)
                 for i in range(self.dim):
                     for k in range(self.dim):
-                        for j in range(self.dim):
+                        for j in range(self.dim):    #again a mess and done in a different manner to tensor product
                             summ[0] += (self.matrix[j+self.dim*i]*other.matrix[k+j*self.dim])
                         new_mat[k+self.dim*i] += summ[0]
                         summ = np.zeros(1,dtype=np.complex128)
@@ -136,7 +135,7 @@ class Gate:
                 return Gate(new_name, new_info, np.array(new_mat))
             else:
                 print(Gate_data.operation_error)
-        elif isinstance(other, Qubit):
+        elif isinstance(other, Qubit):  #splits up based on type as this isnt two n x n but rather n x n and n matrix
             new_name = f"[{self.name}] {other.name}"
             new_mat = np.zeros(self.dim,dtype=np.complex128)
             for i in range(self.dim):
@@ -148,7 +147,7 @@ class Gate:
         else:
             print(Gate_data.operation_error)
     
-    def __add__(self, other):
+    def __add__(self, other):         #direct sum
         if isinstance(other, Gate):
             new_info = "This is a direct sum of gates: "f"{self.name}"" and "f"{other.name}"
             new_name = f"{self.name} + {other.name}"
@@ -156,10 +155,10 @@ class Gate:
             new_length = new_dim**2
             new_mat = np.zeros(new_length,dtype=np.complex128)
             for i in range(self.dim):
-                for j in range(self.dim):
+                for j in range(self.dim):                   #a lot more elegant
                     new_mat[j+new_dim*i] += self.matrix[j+self.dim*i]
-            for i in range(other.dim):
-                for j in range(other.dim):
+            for i in range(other.dim):     #although would be faster if i made a function to apply straight
+                for j in range(other.dim):    #to individual qubits instead
                     new_mat[self.dim+j+self.dim*new_dim+new_dim*i] += other.matrix[j+other.dim*i]
             return Gate(new_name, new_info, np.array(new_mat))
         else:
@@ -174,14 +173,14 @@ class Gate:
     Then we can matrix multiply successive gates together to creat one
     universal matrix that we can apply to the Qubit before measuring""")
 
-class print_array:
-    def __init__(self, array):
-        self.array = array
+class print_array:    #made to try to make matrices look prettier
+    def __init__(self, array):         #probs could have used sp.pretty or whatever but didnt wanna confuse
+        self.array = array             #sp and np matrices and get confused
         prec = 3
         if isinstance(array, Qubit):
             np.set_printoptions(precision=prec,linewidth=20,suppress=True,floatmode="fixed")
             print(array)
-        elif isinstance(array, Gate):
+        elif isinstance(array, Gate):           #so janky
             np.set_printoptions(precision=prec,linewidth=(3+2*(3+prec))*array.dim,suppress=True,floatmode="fixed")
             print(array)
         else:
@@ -205,7 +204,7 @@ Hadamard = Gate("H",Gate_data.Hadamard_info, Gate_data.Hadamard_matrix)
 #print(Hadamard*Hadamard*q1)
 #print(Identity.gate_info)
 
-def Test_Alg(Qubit):
+def Test_Alg(Qubit):         #make sure to mat mult the correct order
     gate1 = X_Gate @ C_Not
     gate2 = Hadamard @ Hadamard @ X_Gate
     gate3 = C_Not @ X_Gate
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # QC-Simulator
-This is a collaboration to simulate a quantum computer
+This is my first attempt at making a Quantum computer simulator
+So far I have commands that can tensor product, matrix multiply and direct sum and have added most needed gates
+My next few goals are to add a universal gate U, add toffoli gates, maybe implement a time evolution.
+Currently, to use you must combine the gates and vectors together.
+Key:
+@: tensor product
+X: matrix multiply
++: direct sum
+H: Hadamard
+X: X gate
+Z: Z gate
+C_Not: C_Not gate
+
