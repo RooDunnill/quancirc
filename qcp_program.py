b49dc0a508ea825ce8b112616f1e6e94a5f782c6
Roo <dunnillt@gmail.com>
2025-03-04 12:56:36 +0000

    just implemented the FWHT to my code, this will allow much much faster Grover searches to a rediculous degree

--- qcp_program.py
+++ qcp_program.py
@@ -444,7 +444,20 @@ class Gate:            #creates a gate class to enable unique properties
             raise QC_error(qc_dat.error_class)
         
     def __mul__(self, other):       #matrix multiplication
-        if isinstance(self, Gate):
+        if isinstance(self, FWHT):
+            for i in range(other.n):
+                step_size = 2**(i + 1)
+                half_step_size = step_size // 2
+                outer_range = np.arange(0, other.dim, step_size)
+                inner_range = np.arange(0, half_step_size)
+                for j in outer_range:
+                    for k in inner_range:
+                        a = other.vector[j + k]
+                        b = other.vector[j + k + half_step_size]
+                        other.vector[j + k] = (a + b) / np.sqrt(2)
+                        other.vector[j + k + half_step_size] = (a - b) / np.sqrt(2)
+            return other
+        elif isinstance(self, Gate):
             if isinstance(other, Gate):    #however probs completely better way to do this so might scrap at some point
                 if self.dim == other.dim:
                     new_mat = np.zeros(self.length,dtype=np.complex128)
@@ -490,6 +503,7 @@ class Gate:            #creates a gate class to enable unique properties
                     return new_mat
             else:
                 raise QC_error(qc_dat.error_mat_dim)
+        
         else:
             raise QC_error(qc_dat.error_class)
     
@@ -519,6 +533,15 @@ class Gate:            #creates a gate class to enable unique properties
         
     def gate_info(self):
         print(qc_dat.gate_info)
+
+class FWHT(Gate):
+    def __init__(self):
+        self.name = f"Fast Walsh Hadamard Transform"
+        self.info = f"An efficient way to apply a Hadamard to every qubit"
+        self.matrix = np.zeros(4)
+        self.length: int = len(self.matrix)
+        self.dim: int = int(np.sqrt(self.length))
+        self.n: int =  0 if self.dim == 0 else int(np.log2(self.dim))
 
 
 class Density(Gate):       #makes a matrix of the probabilities, useful for entangled states
@@ -1144,70 +1167,7 @@ oracle_values2 = [1,2,3,4, 664, 77,5]
 oracle_values3 = [500, 5, 30]
 oracle_values4 = [500, 5, 4, 7, 8, 9, 99]
 def main():
-    rho_ab = Density(state=q1 @ q0 @ q0).rho
-    partial_trace = Density(rho=rho_ab).partial_trace(trace_out="B",state_size=2)
-    print_array(partial_trace)
-    partial_trace2 = Density(rho=rho_ab).partial_trace(trace_out="A",state_size=2)
-    print_array(partial_trace2)
-
-    rho_ab = Density(state=q1 @ q0).rho
-    partial_trace = Density(rho=rho_ab).partial_trace(trace_out="B",state_size=1)
-    print_array(partial_trace)
-    partial_trace2 = Density(rho=rho_ab).partial_trace(trace_out="A",state_size=1)
-    print_array(partial_trace2)
-
-    rho_ab = Density(state=q1 @ q0 @ q0).rho
-    partial_trace = Density(rho=rho_ab).partial_trace(trace_out="B",state_size=1)
-    print_array(partial_trace)
-    partial_trace2 = Density(rho=rho_ab).partial_trace(trace_out="A",state_size=1)
-    print_array(partial_trace2)
-
-    q00 = q0 @ q0
-    q11 = q1 @ q1
-    test_den_object = Density(state_a=q00, state_b=q11, state=q00 @ q11)
-    print_array(test_den_object.rho)
-    print_array(test_den_object.rho_a)
-    print_array(test_den_object.rho_b)
-    print_array(f"Rho B after tracing out A\n{test_den_object.partial_trace(trace_out="A", state_size=2)}")
-    print_array(f"Rho A after tracing out B\n{test_den_object.partial_trace(trace_out="B", state_size=2)}")
-    print_array(f"Trace Distance between A and B: {test_den_object.trace_distance()}")
-    print_array(f"Fidelity between state A and B: {test_den_object.fidelity()}")
-    print_array(f"Quantum Mutual Information S(A:B): {test_den_object.quantum_mutual_info()}")
-    print_array(f"Quantum Conditional Entropy S(A|B): {test_den_object.quantum_conditional_entropy(rho="A")}")
-    print_array(f"Quantum Conditional Entropy S(B|A): {test_den_object.quantum_conditional_entropy(rho="B")}")
-    print_array(f"Quantum Relative Entropy S(A||B): {test_den_object.quantum_relative_entropy(rho="A")}")
-    print_array(f"Quantum Relative Entropy S(B||A): {test_den_object.quantum_relative_entropy(rho="B")}")
-
-    qpp = qp @ qp
-    qmm = qm @ qm
-    test_den_object = Density(state_a=qpp, state_b=qmm, state=qpp @ qmm)
-    print_array(test_den_object.rho)
-    print_array(test_den_object.rho_a)
-    print_array(test_den_object.rho_b)
-    print_array(f"Rho B after tracing out A\n{test_den_object.partial_trace(trace_out="A", state_size=2)}")
-    print_array(f"Rho A after tracing out B\n{test_den_object.partial_trace(trace_out="B", state_size=2)}")
-    print_array(f"Trace Distance between A and B: {test_den_object.trace_distance()}")
-    print_array(f"Fidelity between state A and B: {test_den_object.fidelity()}")
-    print_array(f"Quantum Mutual Information S(A:B): {test_den_object.quantum_mutual_info()}")
-    print_array(f"Quantum Conditional Entropy S(A|B): {test_den_object.quantum_conditional_entropy(rho="A")}")
-    print_array(f"Quantum Conditional Entropy S(B|A): {test_den_object.quantum_conditional_entropy(rho="B")}")
-    print_array(f"Quantum Relative Entropy S(A||B): {test_den_object.quantum_relative_entropy(rho="A")}")
-    print_array(f"Quantum Relative Entropy S(B||A): {test_den_object.quantum_relative_entropy(rho="B")}")
-
-    test_circuit = Circuit(n=3)
-    test_circuit.add_single_gate(gate=Hadamard, gate_location=0)
-    print_array(Hadamard @ Identity @ Identity)
-    test_circuit.add_single_gate(gate=X_Gate, gate_location=1)
-    print_array(Identity @ X_Gate @ Identity)
-    test_circuit.run()
-    print_array(test_circuit.return_info("final_gate"))
-    Grover(oracle_values).run()
-    Grover(oracle_values2).run()
-    Grover(oracle_values3).run()
-    Grover(oracle_values4).run()
-    print_array(q0 @ q0 @ q0 @ q0)
-    print_array(Hadamard * q0)
-    print_array(type(Hadamard * q0))
-    print_array(Hadamard * Hadamard)
-    print_array(Qubit(type="seperable", vectors=[q0,q0,q1]))
-    print_array(Qubit(type="seperable", vectors=[[1,0],[1,0],[0,1]]))
+    
+    F_Qubit = Qubit.q0(n=16)
+    F = FWHT()
+    print_array(F * F_Qubit)
