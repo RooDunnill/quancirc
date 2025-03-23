9de7dfa86f760d54b652194bc9b9e150c6039486
Roo <dunnillt@gmail.com>
2025-03-23 20:44:41 +0000

    i believe i have made a general partial trace function that can trace out two systems at the same time

--- src/scripts/quancirc_run.py
+++ src/scripts/quancirc_run.py
@@ -8,3 +8,15 @@ print(circuit_test.state)
 circuit_test.add_gate(Hadamard, 3)
 print(circuit_test[2])
 circuit_test.list_probs()
+
+
+parital_qub = q1 % q0 % q0 % q1 
+parital_qub.set_display_mode("density")
+new_qub = parital_qub.partial_trace_gen(1,1)
+print(new_qub)
+print("X" * 10)
+new_qub = parital_qub.partial_trace_gen(2,0)
+print(new_qub)
+print("X" * 10)
+new_qub = parital_qub.partial_trace_gen(0,2)
+print(new_qub)
