d48f565ca50f3411b0e940954379a45a7b8448bc
Roo <dunnillt@gmail.com>
2025-03-25 13:31:47 +0000

    created a grover search example file with 3 examples

--- src/examples/__init__.py
+++ src/examples/__init__.py
@@ -1,2 +1,3 @@
 from .mixed_and_pure import *
 from .bell_state_circuit import *
+from .grovers_search_example import *
---
+++ src/examples/grovers_search_example.py
@@ -0,0 +1,16 @@
+from ..circuit_algorithms.grover_search import grover_search
+
+
+def main():
+    print(f"An example of Grovers with 16 random values and 16 qubits:")
+    grover_search(16, n=16)
+
+    print(f"An example of Grovers with 4 set oracle values, 10 iterations and 10 qubits:")
+    oracle_values = [1,3,6,8]
+    grover_search(oracle_values, iterations=10, n=10)
+
+    print(f"An example of Grovers with 8 random oracle values, no set qubit amount and no set iterations")
+    grover_search(8)
+
+if __name__ == "__main__":
+    main()
