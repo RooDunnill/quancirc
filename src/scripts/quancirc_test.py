26d246d02ce19800f8c52880db5b1bf35401f11f
Roo <dunnillt@gmail.com>
2025-03-30 09:37:16 +0100

    added some more tests to quancirc_test

--- src/scripts/quancirc_test.py
+++ src/scripts/quancirc_test.py
@@ -31,3 +31,49 @@ print(LwMeasure(state=qp_lw).measure_state())
 print(LwMeasure(state=qm_lw).measure_state())
 print(LwMeasure(state=qpi_lw).measure_state())
 print(LwMeasure(state=qmi_lw).measure_state())
+normal_qubit_def = Qubit(state=[1,0])
+print(normal_qubit_def)
+normal_lw_qubit_def = LwQubit(state=[1,0])
+print(normal_lw_qubit_def)
+print(Qubit(rho=[[0.5,0],[0,0.5]]))
+print(SymbQubit(state=[1,0]))
+print(CNot @ (normal_lw_qubit_def % q0_lw))
+print(CNot_flip @ (normal_qubit_def % qm))
+normal_mixed_qubit_def = Qubit(state=[[1,0],[0,1],[np.sqrt(0.5),np.sqrt(0.5)]], weights=[0.2,0.5,0.3])
+print(normal_mixed_qubit_def)
+print(Hadamard @ normal_mixed_qubit_def)
+large_qubit_def = q0 % q0 % qm % qm % qp % q0
+large_qubit_state = (Hadamard % Hadamard % Hadamard % Hadamard % Hadamard % Hadamard) @ large_qubit_def
+print(large_qubit_state[0])
+print(large_qubit_state[1])
+print(large_qubit_state[2])
+print(large_qubit_state[3])
+print(large_qubit_state[4])
+print(large_qubit_state[5])
+print(large_qubit_def.partial_trace(0,1))
+print(large_qubit_def.partial_trace(0,2))
+print(large_qubit_def.partial_trace(0,3))
+print(large_qubit_def.partial_trace(0,4))
+print(large_qubit_def.partial_trace(0,5))
+print(large_qubit_def.partial_trace(1,1))
+print(large_qubit_def.partial_trace(2,1))
+print(large_qubit_def.partial_trace(3,1))
+print(large_qubit_def.partial_trace(4,1))
+print(large_qubit_def.partial_trace(0,2))
+print(large_qubit_def.partial_trace(1,2))
+print(large_qubit_def.partial_trace(2,2))
+print(large_qubit_def.partial_trace(3,2))
+print(large_qubit_def.partial_trace(0,3))
+print(large_qubit_def.partial_trace(1,3))
+print(large_qubit_def.partial_trace(2,3))
+print(large_qubit_def.partial_trace(0,4))
+print(large_qubit_def.partial_trace(1,4))
+print(large_qubit_def.partial_trace(0,5))
+large_qubit_def[0] = Hadamard @ large_qubit_def[0]
+large_qubit_def[1] = Hadamard @ large_qubit_def[1]
+large_qubit_def[2] = Hadamard @ large_qubit_def[2]
+large_qubit_def[3] = Hadamard @ large_qubit_def[3]
+large_qubit_def[4] = Hadamard @ large_qubit_def[4]
+
+
+
